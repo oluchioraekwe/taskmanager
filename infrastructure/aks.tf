ef7bd0472d950f30c5c0b68d252c94ea0912bc1d
oluchioraekwe <70038042+oluchioraekwe@users.noreply.github.com>
2024-02-16 10:12:54 +0100

    added modules

--- infrastructure/aks.tf
+++ infrastructure/aks.tf
@@ -1,30 +1,6 @@
-
-resource "azurerm_kubernetes_cluster" "k8s" {
-  location            = azurerm_resource_group.main.location
-  name                = var.cluster_name
-  resource_group_name = azurerm_resource_group.main.name
-  dns_prefix          = "task"
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  default_node_pool {
-    name       = "agentpool"
-    vm_size    = "Standard_D2_v2"
-    node_count = 2
-  }
-
-  automatic_channel_upgrade = "patch"
-  #   linux_profile {
-  #     admin_username = var.username
-
-  #     ssh_key {
-  #       key_data = jsondecode(azapi_resource_action.ssh_public_key_gen.output).publicKey
-  #     }
-  #   }
-  network_profile {
-    network_plugin    = "kubenet"
-    load_balancer_sku = "standard"
-  }
+module "aks-cluster" {
+  source           = "./modules"
+  cluster_location = var.cluster_location
+  cluster_name     = var.cluster_name
+  cluster_rg       = azurerm_resource_group.main.name
 }
--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.7.0"
+      version = "=3.0.0"
     }
   }
 
@@ -25,14 +25,14 @@ terraform {
 #     subscription_id = "416f0a23-14c4-4f97-990a-ebe5379b3d48"
 #     client_id = "bf7cd022-b7cd-4c5d-bd9f-f688ef1f33e6"
 #     tenant_id = "0755a81e-ca57-47de-a8e8-8dc64b559dfc"
-    
+
 #     # use_oidc = true
 # }
 
 
 provider "azurerm" {
   features {}
-  use_oidc        = true
+  # use_oidc        = true
   subscription_id = "416f0a23-14c4-4f97-990a-ebe5379b3d48"
   client_id = "bf7cd022-b7cd-4c5d-bd9f-f688ef1f33e6"
     tenant_id = "0755a81e-ca57-47de-a8e8-8dc64b559dfc"
