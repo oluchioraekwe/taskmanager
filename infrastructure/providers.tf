0eb6ca57a42e877e8d1edab2f90da98a6f56b59b
oluchioraekwe <70038042+oluchioraekwe@users.noreply.github.com>
2024-02-16 10:24:10 +0100

    mend

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -32,7 +32,7 @@ terraform {
 
 provider "azurerm" {
   features {}
-  # use_oidc        = true
+  use_oidc        = true
   subscription_id = "416f0a23-14c4-4f97-990a-ebe5379b3d48"
   client_id = "bf7cd022-b7cd-4c5d-bd9f-f688ef1f33e6"
     tenant_id = "0755a81e-ca57-47de-a8e8-8dc64b559dfc"
