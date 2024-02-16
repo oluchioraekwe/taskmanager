80179d4e7549ff260c1761038ca2a2ba824e37cb
oluchioraekwe <70038042+oluchioraekwe@users.noreply.github.com>
2024-02-16 02:25:19 +0100

    mend

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -12,6 +12,7 @@ terraform {
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
     use_oidc = true
+    subscription_id = "416f0a23-14c4-4f97-990a-ebe5379b3d48"
     client_id = "bf7cd022-b7cd-4c5d-bd9f-f688ef1f33e6"
     tenant_id = "0755a81e-ca57-47de-a8e8-8dc64b559dfc"
   }
