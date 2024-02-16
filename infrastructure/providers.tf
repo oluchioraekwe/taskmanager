3dfd80b13dbe0cdfcec5d39c060d01766d2a5d85
oluchioraekwe <70038042+oluchioraekwe@users.noreply.github.com>
2024-02-16 02:16:17 +0100

    mend

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -12,6 +12,8 @@ terraform {
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
     use_oidc = true
+    subscription_id      = "416f0a23-14c4-4f97-990a-ebe5379b3d48"
+    tenant_id            = "0755a81e-ca57-47de-a8e8-8dc64b559dfc"
   }
 }
 
