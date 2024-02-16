2bc0a1693f5b45d6a5e75d785f40bce795e8552c
oluchioraekwe <70038042+oluchioraekwe@users.noreply.github.com>
2024-02-16 02:37:50 +0100

    mend

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -21,6 +21,9 @@ terraform {
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-  # use_oidc = true
   features {}
+    subscription_id = "416f0a23-14c4-4f97-990a-ebe5379b3d48"
+    client_id = "bf7cd022-b7cd-4c5d-bd9f-f688ef1f33e6"
+    tenant_id = "0755a81e-ca57-47de-a8e8-8dc64b559dfc"
+    # use_oidc = true
 }
