832d86f56b98e7f047fdc3ebf6c1af8a1b0b530c
oluchioraekwe <70038042+oluchioraekwe@users.noreply.github.com>
2024-02-16 02:18:55 +0100

    mend

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -2,24 +2,20 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+      version = "3.75.0"
     }
   }
-
-    backend "azurerm" {
+  backend "azurerm" {
     resource_group_name  = "storage"
     storage_account_name = "aksstore"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
-    use_oidc = true
-    subscription_id      = "416f0a23-14c4-4f97-990a-ebe5379b3d48"
-    tenant_id            = "0755a81e-ca57-47de-a8e8-8dc64b559dfc"
+    use_oidc             = true
   }
 }
 
-
-# Configure the Microsoft Azure Provider
 provider "azurerm" {
   # use_oidc = true
+  # Configuration options
   features {}
 }
